f897b5d0dd59ea2f0e11a07d20c200c67ea8d11f
L250833@lhr.nu.edu.pk <masgharimamsyed@gmail.com>
2026-07-16 17:48:23 +0500

    starting and running screen

--- app.js
+++ app.js
@@ -33,32 +33,90 @@ searchInput.addEventListener('keydown', clickfunction);
 
 /* functions for event listener */
 async function clickfunction() {
-    if (event.type == 'click' || event.key == "Enter") {
-        if (searchInput.value != "") {
-            if (!screenStart) {
-                startingScreen.classList.add('noDisplay');
-                screenStart = true;
+    if ((event.type == 'click' || event.key == "Enter") && (searchInput.value != "")) {
+        if (!screenStart) {
+            startingScreen.classList.add('noDisplay');
+            screenStart = true;
+        }
+        let result;
+        try {
+            let data = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&appid=c8d016794fe74b5af611d5bc42748eaf&units=metric`);
+            result = await data.json();
+        }
+        catch (err) {
+            console.log("error is happening while fetching data form API:", err);
+        }
+
+        if (result.cod == 404) {
+            notFoundScreen.classList.remove('noDisplay');
+            runningScreen.classList.add('noDisplay');
+        }
+        else if (result.cod == 200) {
+            notFoundScreen.classList.add('noDisplay');
+            runningScreen.classList.remove('noDisplay');
+            console.log(result);
+            /* Present temp */
+            locationName.innerText = result.city.name;
+            date.innerText = result.list["0"].dt_txt;
+            tempNo.innerText = result.list["0"].main.temp + '°C';
+            tempName.innerText = result.list['0'].weather["0"].main;
+            humidity.innerText = result.list['0'].main.humidity;
+            speed.innerText = result.list['0'].wind.speed;
+
+            let weatherID = result.list['0'].weather['0'].id;
+            if (weatherID >= 200 && weatherID <= 232) {
+                mainImage.src = 'thunderstorm.svg'
             }
-            let result;
-            try {
-                let data = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&appid=c8d016794fe74b5af611d5bc42748eaf&units=metric`);
-                result = await data.json();
+            else if (weatherID >= 300 && weatherID <= 321) {
+                mainImage.src = 'drizzle.svg'
             }
-            catch (err) {
-                console.log("error is happening while fetching data form API:", err);
+            else if (weatherID >= 500 && weatherID <= 531) {
+                mainImage.src = 'rain.svg'
+            }
+            else if (weatherID >= 600 && weatherID <= 622) {
+                mainImage.src = 'snow.svg'
+            }
+            else if (weatherID >= 801 && weatherID <= 804) {
+                mainImage.src = 'clouds.svg'
+            }
+            else {
+                mainImage.src = 'clear.svg'
             }
 
-            if (result.cod == 404) {
-                notFoundScreen.classList.remove('noDisplay');
-                runningScreen.classList.add('noDisplay');
+            /* forecase in cards */
+
+            for (let i = 1; i <= 10; i++) {
+                console.log(i);
+                let headingDate = document.querySelector(`.card${i} h6`);
+                let cardImg = document.querySelector(`.card${i} img`);
+                let headingTemp = document.querySelector(`.card${i} h5`);
+
+                headingDate.innerText = result.list[`${i}`].dt_txt;
+                headingTemp.innerText = result.list[`${i}`].main.temp + '°C';
+
+                let weatherID = result.list[`${i}`].weather['0'].id;
+                if (weatherID >= 200 && weatherID <= 232) {
+                    cardImg.src = 'thunderstorm.svg'
+                }
+                else if (weatherID >= 300 && weatherID <= 321) {
+                    cardImg.src = 'drizzle.svg'
+                }
+                else if (weatherID >= 500 && weatherID <= 531) {
+                    cardImg.src = 'rain.svg'
+                }
+                else if (weatherID >= 600 && weatherID <= 622) {
+                    cardImg.src = 'snow.svg'
+                }
+                else if (weatherID >= 801 && weatherID <= 804) {
+                    cardImg.src = 'clouds.svg'
+                }
+                else {
+                    cardImg.src = 'clear.svg'
+                }
             }
-            else if (result.cod == 200) {
-                notFoundScreen.classList.add('noDisplay');
-                runningScreen.classList.remove('noDisplay');
-                console.log(result);
-            }
-            searchInput.value = "";
         }
+        searchInput.value = "";
+
     }
 
 
